f3af284c478ced61444a2a4e8a76af1baa9b9e15
Ellen Van Steenberge <ellen.vstb@gmail.com>
2023-09-12 15:05:33 +0200

    first

---
+++ 4_controlestructuur/enkelvoudigeSelectie/js/index.js
@@ -0,0 +1,15 @@
+    let getal1 = prompt("Geef een eerste getal in:");
+    let getal2 = prompt("Geef een tweede getal in:");
+
+    if(getal1 >= getal2){
+    //true
+    console.log(getal1 + " is groter of gelijk aan " + getal2);
+}else{ //false
+    console.log(getal1 + " is kleiner dan " + getal2);
+
+}
+
+    // KORTE VERSIE
+    //     getal1>=getal2 ?
+    //     console.log(getal1 + " is groter of gelijk aan " + getal2);
+    //     console.log(getal1 + " is kleiner dan " + getal2);
